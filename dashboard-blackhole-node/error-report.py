c898388f86c87fb3b54b26e2e7b1c1b08e8788b5
Samir Cury <samir@cern.ch>
2013-07-23 23:08:11 +0200

    already finds black hole nodes that fail > TH%

--- dashboard-blackhole-node/error-report.py
+++ dashboard-blackhole-node/error-report.py
@@ -9,33 +9,69 @@ import time
 
 # Get all failures
 
-hoursOffset = 1
+hoursOffset = 12 
 
-finalTime = time.strftime("%Y-%m-%d+%H")+"%3A"+time.strftime("%MA00")
-initTime = time.strftime("%Y-%m-%d+")+str(int(time.strftime("%H"))-hoursOffset)+"%3A"+time.strftime("%MA00")
+def failDistLast(hours):
 
-dashbUrl = "http://dashb-cms-job.cern.ch/dashboard/request.py/jobstatus2?user=&site=T2_US_Caltech&submissiontool=&application=&activity=&status=aborted&check=terminated&tier=&sortby=user&ce=&rb=&grid=&jobtype=&submissionui=&dataset=&submissiontype=&task=&subtoolver=&genactivity=production&outputse=&appexitcode=&accesstype=&date1=%s&date2=%s&count=1634&offset=0&exitcode=&fail=&cat=&len=5000&prettyprint" % (initTime, finalTime)
-
-print dashbUrl
-
-response = urllib2.urlopen(dashbUrl)
-
-# check 200 or bail
-
-dashbjson = json.loads(response.read())
-
-failureDistribution = {}
-#print dashbjson['jobs']
-for job in dashbjson['jobs']:
-	if failureDistribution.has_key(job['WNHostName']):
-		failureDistribution[job['WNHostName']] += 1
-	else :
-		failureDistribution[job['WNHostName']] = 1
-
-print failureDistribution
-
+	finalTime = time.strftime("%Y-%m-%d+%H")+"%3A"+time.strftime("%M")
+	initTime = time.strftime("%Y-%m-%d+")+str(int(time.strftime("%H"))-hoursOffset)+"%3A"+time.strftime("%M")
+	
+	#print "%s %s" % (initTime, finalTime)
+	
+	dashbUrl = "http://dashb-cms-job.cern.ch/dashboard/request.py/jobstatus2?user=&site=T2_US_Caltech&submissiontool=&application=&activity=analysis&status=app-failed&check=terminated&tier=&sortby=activity&ce=&rb=&grid=&jobtype=&submissionui=&dataset=&submissiontype=&task=&subtoolver=&genactivity=&outputse=&appexitcode=&accesstype=&date1=%s&date2=%s&count=9999&offset=0&exitcode=all&fail=all&cat=&len=5000&prettyprint" % (initTime, finalTime)
+	
+	response = urllib2.urlopen(dashbUrl)
+	
+	# check 200 or bail
+	
+	dashbjson = json.loads(response.read())
+	
+	
+	failureDistribution = {}
+	#print dashbjson['jobs']
+	for job in dashbjson['jobs']:
+		if failureDistribution.has_key(job['WNHostName']):
+			failureDistribution[job['WNHostName']] += 1
+		else :
+			failureDistribution[job['WNHostName']] = 1
+	
+	return failureDistribution
 
 #print dashbjson
+
+def findAvgFail(failDist):
+	total = 0
+	for node in failDist.keys():
+		total += failDist[node]
+	avg = total/len(failDist.keys())
+	return avg
+
+def findTotalFail(failDist):
+	total = 0
+	for node in failDist.keys():
+	       total += failDist[node]
+	return total
+
+
+def findBadNodes(failDist) :
+	badNodes = []
+	totalFail = findTotalFail(failDist)
+	print "aaaaa %d" % totalFail
+	th = 0.1
+        for node in failDist.keys():
+		failShare = float(failDist[node])/float(totalFail)
+		if failShare > th:
+			print "node %s is bad with %f failures" % (node, failShare)
+			
+
+failDist = failDistLast(hoursOffset)
+
+for node in failDist.keys():
+	print "%s had %d failures" % (node, failDist[node])
+
+print "average failure rate is %d" % findAvgFail(failDist)
+
+findBadNodes(failDist)
 
 
 
